fe40bfdae225bcefe260809889698eb35d76677c
Redion Xhepa <redionxhepa@outlook.com>
2023-06-11 20:08:12 +0200

    Intial commit

---
+++ src/main/java/LinkValidatorSync.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class LinkValidatorSync {
+
+    private static HttpClient client;
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    private static String validateLink(String link) {
+        return null;
+    }
+
+    private static String responseToString(HttpResponse<Void> response) {
+        int status = response.statusCode();
+        boolean success = status >= 200 && status <= 299;
+        return String.format("%s -> %s (status: %s)", response.uri(), success, status);
+    }
+}
